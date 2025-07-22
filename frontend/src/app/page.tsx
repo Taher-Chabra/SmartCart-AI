62bd436c3adac1fea46fa9a7c46cc439aac4eeab
Taher Chabra <redsign12345@gmail.com>
2025-07-22 22:40:24 +0530

    Initial Project Setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Welcome to SmartCart - AI</h1>
+      <p className="mt-4 text-lg">
+        This is a simple Next.js application.
+      </p>
+    </div>
+  );
+}
