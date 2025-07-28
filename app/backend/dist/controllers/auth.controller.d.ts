70b3886c9db6b8855d1a3d51874d97f51bd7c133
Taher Chabra <redsign12345@gmail.com>
2025-07-28 22:25:56 +0530

    feat: Add build outputs to .gitignore and untrack existing ones

--- app/backend/dist/controllers/auth.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-declare const registerUser: (req: Request, res: Response, next: import("express").NextFunction) => void;
-declare const localUserLogin: (req: Request, res: Response, next: import("express").NextFunction) => void;
-declare const googleUserLogin: (req: Request, res: Response, next: import("express").NextFunction) => void;
-export { registerUser, localUserLogin, googleUserLogin };
--- app/backend/dist/utils/tokens.utils.d.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-export declare const generateSecretTokens: (userId: mongoose.Types.ObjectId) => Promise<{
-    accessToken: string;
-    refreshToken: string;
-}>;
